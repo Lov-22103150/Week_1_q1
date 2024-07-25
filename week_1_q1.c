d462f473e12f48133963d5be94026e6caa7644f9
Lov-22103150 <22103150@mail.jiit.ac.in>
2024-07-25 15:35:22 +0530

    Add files via upload

---
+++ week_1_q1.c
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int size;
+    cout<<"enter the size of array :"<<endl;
+    cin>>size;
+    cout<<"enter the element in array :"<<endl;
+    int *arr=new int[size];
+    for(int i=0;i<n;i++){
+            cin>>arr[i];
+    }
+    for(int i=0;i<size;i++){
+        cout<<arr[i];
+    }
+    return 0;
+
+}
+
